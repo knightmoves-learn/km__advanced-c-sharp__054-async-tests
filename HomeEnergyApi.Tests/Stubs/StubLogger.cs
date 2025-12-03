af77c7a798ee61e65380d89b5efcee4deb5dd490
ChrisAndersonKM <chris.anderson@knightmoves.org>
2025-12-03 11:59:00 -0600

    updated instructions

--- HomeEnergyApi.Tests/Stubs/StubLogger.cs
+++ HomeEnergyApi.Tests/Stubs/StubLogger.cs
@@ -3,13 +3,17 @@ using Microsoft.Extensions.Logging;
 class StubLogger<T> : ILogger<T>
 {
     public List<string> LoggedInfoMessages { get; } = new List<string>();
-    
+    public List<string> LoggedDebugMessages { get; } = new List<string>(); 
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
     {
         if (logLevel == LogLevel.Information)
         {
             LoggedInfoMessages.Add(formatter(state, exception));
+        }        
+        if (logLevel == LogLevel.Debug)
+        {
+            LoggedDebugMessages.Add(formatter(state, exception));
         }
     }
 
